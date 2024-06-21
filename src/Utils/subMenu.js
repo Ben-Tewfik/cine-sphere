292b4fcf7905301983e0655e917d1e7ec0baefdc
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-21 01:49:41 +0100

    create submenu data to display it in the submenu

---
+++ src/Utils/subMenu.js
@@ -0,0 +1,26 @@
+export const subMenu = [
+  {
+    page: "Movies",
+    links: [
+      { id: 1, name: "Top Rated" },
+      { id: 2, name: "Latest" },
+      { id: 3, name: "Popular" },
+      { id: 4, name: "Now Playing" },
+      { id: 5, name: "Upcoming" },
+    ],
+  },
+  {
+    page: "Genres",
+    links: [],
+  },
+  {
+    page: "TV-Series",
+    links: [
+      { id: 1, name: "Top Rated" },
+      { id: 2, name: "Latest" },
+      { id: 3, name: "Popular" },
+      { id: 4, name: "Now Playing" },
+      { id: 5, name: "Upcoming" },
+    ],
+  },
+];
