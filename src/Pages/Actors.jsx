a29b3298dfa28a10f74013538865e44e87080161
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-22 03:06:08 +0100

    display and style all the actors in the actors page

--- src/Pages/Actors.jsx
+++ src/Pages/Actors.jsx
@@ -1,3 +1,16 @@
+import { useGlobalContext } from "../Context/AppContext";
+import ActorCard from "../components/ActorCard/ActorCard";
+import Loader from "../components/Loader/Loader";
 export default function Actors() {
-  return <div>Actors</div>;
+  const { actors, isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <Loader />;
+  }
+  return (
+    <section className="w-[90vw] mx-auto py-10 grid gap-6 grid-cols-auto-fill">
+      {actors?.map(actor => {
+        return <ActorCard key={actor.id} {...actor} />;
+      })}
+    </section>
+  );
 }
