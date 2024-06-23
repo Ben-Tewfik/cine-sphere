53773f844621ddc3b24742f3965be47517f4d954
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-23 03:42:58 +0100

    map through the fetched movies after passing them from the context into the movies page

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,3 +1,17 @@
+import { useGlobalContext } from "../Context/AppContext";
+import Card from "../components/Card/Card";
+import Loader from "../components/Loader/Loader";
+
 export default function Movies() {
-  return <div>Movies</div>;
+  const { movies, isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <Loader />;
+  }
+  return (
+    <section className="w-[90vw] mx-auto py-10 grid gap-6 grid-cols-auto-fill">
+      {movies?.map(item => {
+        return <Card key={item.id} {...item} />;
+      })}
+    </section>
+  );
 }
