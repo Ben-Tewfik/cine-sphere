0f53910ff064e471b258096905fe360f368a31e9
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-23 03:41:39 +0100

    add a link to card component to navigate the user to the single movie page and add a default image incase fetched image is null

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,26 @@
-export default function Card({ poster_path, original_title, title }) {
+import { Link } from "react-router-dom";
+import { noImage } from "../../../public/Images";
+export default function Card({ id, poster_path, title }) {
   return (
-    <article
+    <Link
+      to={`/movies/${id}`}
       title={title}
       className="group relative rounded-md overflow-hidden"
     >
       <img
-        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        src={
+          poster_path === null
+            ? noImage
+            : `https://image.tmdb.org/t/p/w500${poster_path}`
+        }
         alt={`${title} Poster`}
-        className="bloc"
+        className={`${
+          poster_path === null ? "block h-[100%] object-cover" : "block"
+        }`}
       />
       <div className="absolute bottom-0 left-0 w-[100%] bg-[#00000099] py-2 px-4 text-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-300 ease-in-out">
-        <h4 className="text-white">{original_title}</h4>
+        <h4 className="text-white">{title}</h4>
       </div>
-    </article>
+    </Link>
   );
 }
