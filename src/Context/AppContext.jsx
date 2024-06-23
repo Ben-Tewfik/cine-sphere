1655def36b220cd8ea8c99eaf2c6f28f934c2802
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-23 03:39:40 +0100

    create loader and add loading logic in appContext

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -3,20 +3,22 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { subMenu } from "../Utils/subMenu";
 const AppProvider = createContext();
 // const key = `${import.meta.env.REACT_APP_API_KEY}`;
-
+export const key = `?api_key=${import.meta.env.VITE_API_KEY}`;
+export const baseURL = `https://api.themoviedb.org/3/`;
+const peopleURL = `${baseURL}person/popular${key}&language=en-US&page=1`;
+export const baseImgUrl = "https://image.tmdb.org/t/p/w500";
 export default function AppContext({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
   const [location, setLocation] = useState({});
   const [page, setPage] = useState([]);
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
+  const [actors, setActors] = useState([]);
+  const [movieQuery, setMovieQuery] = useState("trending/movie/day");
 
-  const key = `?api_key=${import.meta.env.VITE_API_KEY}`;
-  const baseURL = `https://api.themoviedb.org/3/`;
-  const url = `${baseURL}trending/movie/day${key}`;
-
-  const fetchData = async () => {
-    // fetch Genres
+  // fetch Genres
+  const fetchGenre = async () => {
     try {
       const {
         data: { genres },
@@ -25,19 +27,45 @@ export default function AppContext({ children }) {
     } catch (error) {
       console.error(error);
     }
-    // fetch movies
+  };
+
+  // fetch movies
+  const fetchMovie = async () => {
+    setIsLoading(true);
     try {
       const {
         data: { results },
-      } = await axios(url);
+      } = await axios(`https://api.themoviedb.org/3/${movieQuery}${key}`);
+
       setMovies(results);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
+    }
+  };
+  // fetchPeople
+  const fetchPeople = async () => {
+    setIsLoading(true);
+    try {
+      const {
+        data: { results },
+      } = await axios(peopleURL);
+      setActors(results);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
     }
   };
   useEffect(() => {
-    fetchData();
+    fetchGenre();
+    fetchPeople();
   }, []);
+  useEffect(() => {
+    fetchMovie();
+  }, [movieQuery]);
+
   // functions for submenu
   function openSubMenu(page, coordinates) {
     const desiredPage = subMenu.find(item => item.page === page);
@@ -63,6 +91,9 @@ export default function AppContext({ children }) {
         genres,
         isSubMenuOpen,
         movies,
+        actors,
+        setMovieQuery,
+        isLoading,
       }}
     >
       {children}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <section className="w-screen h-screen flex justify-center items-center">
+      <div className="h-24 w-24 rounded-full border-t-[#ff601c] border-8 border-gray-200 animate-spin"></div>
+    </section>
+  );
+}
