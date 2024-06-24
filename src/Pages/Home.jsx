8d9f63cf28b11493f0087fbb37a44e2d219f8d3a
benarba mohammed <tewfikhotspur@gmail.com>
2024-06-24 11:53:31 +0100

    add a section to display the a fiew trending movies and tvshows in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,40 @@
+import { Link } from "react-router-dom";
 import { useGlobalContext } from "../Context/AppContext";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import Card from "../components/Card/Card";
 
 export default function Home() {
-  // const { test } = useGlobalContext();
+  const { trendingMovies } = useGlobalContext();
 
-  return <div>Home</div>;
+  return (
+    <section className="text-white w-[90vw] mx-auto py-10">
+      <div className="mb-8 md:flex justify-between items-center gap-4">
+        <h2 className="text-2xl mb-4 md:mb-0">Trending</h2>
+        <div className="flex justify-between items-center flex-1">
+          <div className="flex gap-4">
+            <button className="border-2 border-[#ff601c] py-1 px-3 rounded-lg text-[#ff601c] font-semibold hover:text-white hover:bg-[#ff601c] transition-colors duration-300 ease-in-out">
+              Movies
+            </button>
+            <button className="border-2 border-[#ff601c] py-1 px-3 rounded-lg text-[#ff601c] font-semibold hover:text-white hover:bg-[#ff601c] transition-colors duration-300 ease-in-out">
+              TV Shows
+            </button>
+          </div>
+          <Link
+            to={"/movies"}
+            className="flex items-center justify-self-end text-[#ff601c] font-semibold"
+          >
+            View More <MdKeyboardArrowRight className="text-xl" />
+          </Link>
+        </div>
+      </div>
+      <div className="grid gap-6 grid-cols-auto-fill">
+        {trendingMovies
+          ?.sort((a, b) => b.vote_average - a.vote_average)
+          ?.slice(0, 12)
+          .map(item => {
+            return <Card key={item.id} {...item} />;
+          })}
+      </div>
+    </section>
+  );
 }
