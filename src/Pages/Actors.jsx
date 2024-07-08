a3c8b0e07ad6ebf03610562f89594f1a7b03aaef
benarba mohammed <tewfikhotspur@gmail.com>
2024-07-08 13:46:41 +0100

    add pagination in actors page

--- src/Pages/Actors.jsx
+++ src/Pages/Actors.jsx
@@ -1,6 +1,7 @@
 import { useGlobalContext } from "../Context/AppContext";
 import ActorCard from "../components/ActorCard/ActorCard";
 import Loader from "../components/Loader/Loader";
+import Pagination from "../components/Pagination/Pagination";
 export default function Actors() {
   const { actors, isLoading, closeSubMenu } = useGlobalContext();
   function hideSubMenu() {
@@ -10,13 +11,13 @@ export default function Actors() {
     return <Loader />;
   }
   return (
-    <section
-      onMouseOver={hideSubMenu}
-      className="w-[90vw] mx-auto py-10 grid gap-6 grid-cols-auto-fill"
-    >
-      {actors?.map(actor => {
-        return <ActorCard key={actor.id} {...actor} />;
-      })}
+    <section onMouseOver={hideSubMenu}>
+      <div className="w-[90vw] mx-auto py-10 grid gap-6 grid-cols-auto-fill">
+        {actors?.map(actor => {
+          return <ActorCard key={actor.id} {...actor} />;
+        })}
+      </div>
+      <Pagination />
     </section>
   );
 }
