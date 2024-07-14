c8ebd5e2c2507e544dde2399dc946a057fe508be
benarba mohammed <tewfikhotspur@gmail.com>
2024-07-14 23:47:46 +0100

    add functionality of dropdown to the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import { FaTimes } from "react-icons/fa";
 import { useGlobalContext } from "../../Context/AppContext";
 import { Link } from "react-router-dom";
+import { subMenu } from "../../Utils/subMenu";
+import { useState } from "react";
 
 export default function Sidebar() {
   const { closeSidebar, isOpenSidebar } = useGlobalContext();
+  const [isActive, setIsActive] = useState("");
   return (
     <section
       className={`${
@@ -18,11 +21,36 @@ export default function Sidebar() {
       >
         <FaTimes />
       </button>
-      <ul>
+      <ul className="capitalize">
         <li>
           <Link onClick={closeSidebar} to={"/"}>
             Home
           </Link>
+        </li>
+        {subMenu.map(list => {
+          const { page, links } = list;
+          return (
+            <li
+              onClick={() =>
+                isActive !== page ? setIsActive(page) : setIsActive("")
+              }
+              key={page}
+            >
+              {page}
+              {links.map(link => {
+                return (
+                  <li
+                    className={`${isActive === page ? "block" : "hidden"}`}
+                    key={link.id}
+                  >
+                    {link.name}
+                  </li>
+                );
+              })}
+            </li>
+          );
+        })}
+        <li>
           <Link onClick={closeSidebar} to={"/actors"}>
             Actors
           </Link>
